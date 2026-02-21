dd4d0fb1c1f6bdd57db348df3760c1793bfe9361
Shambhavisharma13 <shambhavisharma8235@gmail.com>
2026-02-21 19:37:40 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your size for Array");
+
+        int n=s.nextInt();
+        int arr[]=new int[n];
+
+        arr[0]=10;
+        arr[1]=20;
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+
+
+
+    }
+    
+}
+
